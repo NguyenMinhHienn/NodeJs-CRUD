bd8e598dcf09358dc2000c5017357dd739a47784
datlt2306 <datlt2306@gmail.com>
2025-01-08 09:52:32 +0700

    add endpoint to serve fake product data

--- app.js
+++ app.js
@@ -1,8 +1,16 @@
 import express from "express";
-
+// framework
 const app = express();
 
-app.listen(() => {
-    console.log("Server is running!");
+// data fake
+const data = [
+    { id: 1, name: "Product A", price: 100 },
+    { id: 2, name: "Product B", price: 200 },
+    { id: 3, name: "Product C", price: 300 },
+    { id: 4, name: "Product D", price: 400 },
+];
+
+app.get("/products", (req, res) => {
+    res.json(data);
 });
 export const viteNodeApp = app;
