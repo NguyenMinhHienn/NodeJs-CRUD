89b8de996f3827591546bd0207e26a9dadc36866
datlt2306 <datlt2306@gmail.com>
2025-01-08 11:03:49 +0700

    add POST endpoint to create new products and implement input validation

--- app.js
+++ app.js
@@ -1,7 +1,12 @@
 import express from "express";
+import bodyParser from "body-parser"; // Middleware để parse request body
+
 // framework
 const app = express();
 
+// middleware
+
+app.use(express.json());
 // data fake
 const data = [
     { id: 1, name: "Product A", price: 100 }, // item
@@ -10,9 +15,12 @@ const data = [
     { id: 4, name: "Product D", price: 400 },
 ];
 
+// Lấy danh sách sản phẩm
 app.get("/products", (req, res) => {
     res.status(200).json(data);
 });
+
+// Lấy thông tin sản phẩm theo ID
 app.get("/products/:id", (req, res) => {
     const id = req.params.id;
     const product = data.find((item) => item.id === +id);
@@ -23,5 +31,22 @@ app.get("/products/:id", (req, res) => {
     }
     return res.status(200).json(product);
 });
+
+// Thêm sản phẩm mới
+app.post("/products", (req, res) => {
+    const { id, name, price } = req.body;
+    // // Kiểm tra thông tin đầu vào
+    if (!id || !name || !price) {
+        return res.status(400).json({ message: "Thiếu thông tin sản phẩm!" });
+    }
+    // Kiểm tra xem sản phẩm có trùng ID không
+    const existProduct = data.find((item) => item.id === +id);
+    if (existProduct) return res.status(400).json({ message: "Sản phẩm trùng ID!" });
+    // // Thêm sản phẩm vào danh sách
+
+    const newProduct = { id, name, price };
+    data.push(newProduct);
+    return res.status(201).json(newProduct);
+});
+
 export const viteNodeApp = app;
-// Nhìn - Hiểu -> Nhớ -> coding
