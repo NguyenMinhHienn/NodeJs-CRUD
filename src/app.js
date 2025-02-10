940a7e10db75641706317733770bfea4ed1c3773
datlt2306 <datlt2306@gmail.com>
2025-02-10 07:20:35 +0700

    refactor: remove unused authentication and product routes, models, and controllers

--- src/app.js
+++ src/app.js
@@ -1,17 +1,10 @@
 import express from "express";
-// what? 1 framework nodejs
-// why? nhanh, nhẹ, dễ sử dụng
-// when? khi cần xây dựng 1 ứng dụng web
 import mongoose from "mongoose";
-import productRouter from "./routers/product";
-import authRouter from "./routers/auth";
 const app = express();
 app.use(express.json());
 // Kết nối db
-mongoose.connect(`mongodb://localhost:27017/wd19321`);
+mongoose.connect(`mongodb://localhost:27017/reviews`);
 
 // routers
-app.use("/api", productRouter);
-app.use("/api", authRouter);
 
 export const viteNodeApp = app;
